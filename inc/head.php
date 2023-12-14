9c8ecb69a0b289fbf1b2a2b1d862d65605752684
Rafian Sagar <35956615+Rafiansagar@users.noreply.github.com>
2023-12-15 00:57:26 +0600

    sdas

---
+++ inc/head.php
@@ -0,0 +1,11 @@
+<!-- Meta Tags -->
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="robots" content="noindex, follow" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Meta Tags -->
+<title>Login Register PHP</title>
+
+<!-- Main Sctyle CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
