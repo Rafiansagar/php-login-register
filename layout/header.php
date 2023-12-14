9452ff9e4cda96eabaf2bbc98db4014d4dbc5547
Rafian Sagar <35956615+Rafiansagar@users.noreply.github.com>
2023-12-15 00:56:51 +0600

    ghg

---
+++ layout/header.php
@@ -0,0 +1,9 @@
+<ul>
+    <li><a href="main_content.php">Home</a></li>
+    <li><a href="index.php">login</a></li>
+    <li><a href="logout.php">logout</a></li>
+    <li><a href="signup.php">signup</a></li>
+    <li><a href="blog.php">blog</a></li>
+    <li><a href="create_blog.php">create_blog</a></li>
+    <li><a href="user_test.php">user_test</a></li>
+</ul>
