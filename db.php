0595841d12871a19c6cd51cfb458f5c9457d0212
Rafian Sagar <35956615+Rafiansagar@users.noreply.github.com>
2023-12-15 16:23:26 +0600

    Update db.php

--- db.php
+++ db.php
@@ -4,6 +4,12 @@
     $password = "";
     $dbname = "php_test";
 
+    // Server DB
+    // $servername = "localhost";
+    // $username = "id21674725_root";
+    // $password = "7*******#";
+    // $dbname = "id21674725_php_test";
+
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
