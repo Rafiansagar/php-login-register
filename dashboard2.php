f6d621beffcc986951a70b3d05ae128cfbb86c71
Rafian Sagar <35956615+Rafiansagar@users.noreply.github.com>
2025-04-22 23:33:17 +0600

    sdfdfs

--- dashboard2.php
+++ dashboard2.php
@@ -64,13 +64,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <div class="dash_sidebar col-md-2">
                     <ul class="nav nav-tabs flex-column" role="tablist">
                         <li class="nav-item" role="presentation">
-                            <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#dashboard">
+                            <button class="nav-link" data-bs-toggle="tab" data-bs-target="#dashboard">
                                 <i class="ri-home-2-line"></i>
                                 Dashboard
                             </button>
                         </li>
                         <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="users-tab" data-bs-toggle="tab" data-bs-target="#users"><i class="ri-user-6-line"></i> Users</button>
+                            <button class="nav-link active" id="users-tab" data-bs-toggle="tab" data-bs-target="#users"><i class="ri-user-6-line"></i> Users</button>
                         </li>
                         <li class="nav-item" role="presentation">
                             <button class="nav-link" id="recycle-bin-tab" data-bs-toggle="tab" data-bs-target="#recycle-bin"><i class="ri-delete-bin-line"></i> Recycle Bin</button>
@@ -83,13 +83,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                                 <button type="submit"><i class="ri-logout-box-line"></i>Logout</button>
                             </form>
                         </li>
-                        <li><a href="/main_content.php" target="_blank"><i class="ri-computer-line"></i>Visit Site</a></li>
+                        <li><a href="main_content.php" target="_blank"><i class="ri-computer-line"></i>Visit Site</a></li>
                     </ul>
                 </div>
                 <div class="dash_content col-md-10">
                     <div class="tab-content">
                         <!-- Dashboard Content Start -->
-                        <div class="tab-pane fade show active" id="dashboard">
+                        <div class="tab-pane fade" id="dashboard">
                             <div class="welcome-note">
                                 <h2 class="welcome">Welcome</h2>
                                 <div class="name"><i class="ri-user-line"></i><span><?php echo $username; ?></span></div>
@@ -98,25 +98,62 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                         <!-- Dashboard Content End -->
 
                         <!-- Intro Content Start -->
-                        <div class="tab-pane fade" id="users">
-                            <?php if (isset($_SESSION["role"])) {
-                                $role = $_SESSION["role"];
-                                echo "Name: $username<br>";
-                                echo "Role: $role!<br>";
-                                
-                                $userInfoQuery = "SELECT * FROM admin_users WHERE username = '$username'";
-                                $result = $conn->query($userInfoQuery);
+                        <div class="tab-pane fade show active" id="users">
+                            <div class="tab-banner current-user">
+                                <?php if (isset($_SESSION["role"])) {
+                                    $role = $_SESSION["role"];
+                                    echo "Name: $username<br>";
+                                    echo "Role: $role!<br>";
+                                    
+                                    $userInfoQuery = "SELECT * FROM admin_users WHERE username = '$username'";
+                                    $result = $conn->query($userInfoQuery);
 
-                                if ($result->num_rows > 0) {
-                                    $userInfo = $result->fetch_assoc();
-                                    echo "Email: " . $userInfo['email'];
-                                }
-                            } ?>
+                                    if ($result->num_rows > 0) {
+                                        $userInfo = $result->fetch_assoc();
+                                        echo "Email: " . $userInfo['email'];
+                                    }
+                                } ?>
+                            </div>
+
+                            <div class="table_component">
+
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Username</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Role</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php 
+                                            $userListQuery = "SELECT id, role, username, email FROM admin_users";
+                                            $result = $conn->query($userListQuery);
+                                            while ($row = $result->fetch_assoc()) {
+                                                $userName = !empty($row['username']) ? $row['username'] : 'N/A';
+                                                $nickName = !empty($row['user_nick_name']) ? $row['user_nick_name'] : 'N/A';
+                                                $userEmail = !empty($row['email']) ? $row['email'] : 'N/A';
+                                                $userRole = !empty($row['role']) ? $row['role'] : 'N/A';
+                                                ?>
+                                                <tr>
+                                                    <td><?php echo $userName; ?></td>
+                                                    <td><?php echo $nickName; ?></td>
+                                                    <td><?php echo $userEmail; ?></td>
+                                                    <td><?php echo $userRole; ?></td>
+                                                </tr>
+                                            <?php }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                         <!-- Intro Content End -->
 
                         <!-- Recycle Bin Content Start -->
                         <div class="tab-pane fade" id="recycle-bin">
+                            <div class="tab-banner">Recycle Bin</div>
+
                             <div class="row">
                                 <?php
                                     $dir = 'uploads/recyclebin/';
